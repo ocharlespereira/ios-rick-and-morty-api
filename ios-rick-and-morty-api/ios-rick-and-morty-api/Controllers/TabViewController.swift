99fa40bb029b3b57124957ad649e5020f79254dc
Charles Pereira <charlesti.ptu@gmail.com>
2024-07-14 18:30:30 -0300

    chore: add image and large text for tab bar menu

--- ios-rick-and-morty-api/ios-rick-and-morty-api/Controllers/TabViewController.swift
+++ ios-rick-and-morty-api/ios-rick-and-morty-api/Controllers/TabViewController.swift
@@ -4,7 +4,6 @@ final class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
         
         setUpLabs()
     }
@@ -15,10 +14,23 @@ final class TabBarViewController: UITabBarController {
         let episodesVC = EpisodeViewController()
         let settingsVC = SettingsViewController()
         
+        // Text Large
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationsVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        // Set in the root navigation controller
         let nav1 = UINavigationController(rootViewController: charactersVC)
         let nav2 = UINavigationController(rootViewController: locationsVC)
         let nav3 = UINavigationController(rootViewController: episodesVC)
         let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        // add image and view menu
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
         
         setViewControllers(
             [nav1, nav2, nav3, nav4],
