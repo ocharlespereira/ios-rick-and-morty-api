11e3317c18a4f71a2bddd971c133624c295ef0d0
Charles Pereira <charlesti.ptu@gmail.com>
2024-07-17 20:11:47 -0300

    feat: type response

---
+++ ios-rick-and-morty-api/Models/APIResponseTypes/GetAllCharactersResponse.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct GetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [Character]
+    
+}
