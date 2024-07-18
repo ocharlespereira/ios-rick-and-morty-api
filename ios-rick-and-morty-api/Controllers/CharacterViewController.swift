91dfb71377dd404d1b282081191ac8d3dd5d2032
Charles Pereira <charlesti.ptu@gmail.com>
2024-07-18 16:36:03 -0300

    chore: move func setUpView styles

--- ios-rick-and-morty-api/Controllers/CharacterViewController.swift
+++ ios-rick-and-morty-api/Controllers/CharacterViewController.swift
@@ -2,23 +2,25 @@ import UIKit
 
 /// Controller to show and search for screen Chracaters
 final class CharacterViewController: UIViewController {
+    
+    private let characterListView = CharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
             
         view.backgroundColor = .systemBackground
         title = "Characters"
-        
-        Service.shared.execute(.listCharactersRequest, expecting: GetAllCharactersResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("Total: "+String(model.info.count))
-                print("Page result count: "+String(model.results.count))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
-
+        setUpView()
+    }
+    
+    private func setUpView () {
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 
 }
