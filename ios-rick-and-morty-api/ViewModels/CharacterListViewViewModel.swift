9f91c8272206bf3cbbd4945435160b497d762087
Charles Pereira <charlesti.ptu@gmail.com>
2024-07-18 16:36:28 -0300

    chore: add rules cell animation

--- ios-rick-and-morty-api/ViewModels/CharacterListViewViewModel.swift
+++ ios-rick-and-morty-api/ViewModels/CharacterListViewViewModel.swift
@@ -1,6 +1,6 @@
-import Foundation
+import UIKit
 
-struct CharacterListViewViewModel {
+final class CharacterListViewViewModel: NSObject {
     
     func fetchCharacters() {
         Service.shared.execute(.listCharactersRequest, expecting: GetAllCharactersResponse.self) { result in
@@ -12,5 +12,30 @@ struct CharacterListViewViewModel {
                 print(String(describing: error))
             }
         }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate,
+                                      UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        
+        return CGSize(
+            width: width,
+            height: width * 1.5
+        )
     }
 }
